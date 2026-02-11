4005be9d0ae02642db7d45a3b26fcc766695682b
Quinn Villaverde <mqvillaverde@tip.edu.ph>
2026-02-11 22:52:13 +0800

    Add blog page with posts JSON

--- server.js
+++ server.js
@@ -7,17 +7,29 @@ const PORT = process.env.PORT || 3000;
 // Serve static files from the "public" folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Define routes for different pages
+// Home route
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 });
 
+// About route
 app.get('/about', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'about.html'));
 });
 
+// Contact route
 app.get('/contact', (req, res) => {
     res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
+
+// Blog page route
+app.get('/blog', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views', 'blog.html'));
+});
+
+// API route to serve posts.json
+app.get('/api/posts', (req, res) => {
+    res.sendFile(path.join(__dirname, 'data', 'posts.json'));
 });
 
 // Start the server
